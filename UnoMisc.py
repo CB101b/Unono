738d5c28b11c0bc1489716b44eb235c6faeca6f1
Merijn Sandvliet <merijnsandvliet7@gmail.com>
2021-02-10 11:18:56 +0100

    Add card printing

---
+++ UnoMisc.py
@@ -0,0 +1,4 @@
+def capital(word: str):
+	word = list(word)
+	word[0] = word[0].upper()
+	return "".join(word)
--- bnu.py
+++ bnu.py
@@ -1,22 +1,26 @@
 import random
+from UnoMisc import *
 import time
 
 colors = ('red','green','blue','yellow','wild','quad')
 deck = []
 deckPersistent = []
 playerDict={}
+winConditions = {
+	"playerCardCount":0
+}
 
 # generates cards, puts them in a deck
 def genCard(colors, deck, deckPersistent):
 	for color in colors:
-		if color != 'wild' or 'quad':
+		if color == 'wild' or color == 'quad':
+			for _ in range(4):
+				deck.append(color)
+		else:
 			for num in range(10):
 				deck.append((color,num))
 			deck.append((color,'+2'))
 			deck.append((color,'invert'))
-		else:
-			for _ in range(4):
-				deck.append(color)
 	for x in deck: deckPersistent.append(x)
 
 def playerGen(playerDict, amount):
@@ -24,7 +28,36 @@ def playerGen(playerDict, amount):
 		playerNum += 1
 		playerDict.update({"player"+str(playerNum) : []})
 
+def initialDealCards(playerDict, deck):
+	for player in playerDict:
+		for _ in range(7):
+			randomCard = deck[random.randint(0,len(deck))]
+			playerDict[player].append(randomCard)
+			deck.remove(randomCard)
+
 
 genCard(colors, deck, deckPersistent)
+
 amount = int(input("How many players?\n"))
 playerGen(playerDict, amount)
+
+print("Okay, let's play!")
+time.sleep(1)
+
+print("Dealing...")
+initialDealCards(playerDict,deck)
+time.sleep(2)
+
+# main game loop
+finished = 0
+while not finished:
+	for player in playerDict:
+		playerCards = playerDict[player]
+		print("It's {}'s turn.".format(player))
+		print("Available cards: ")
+		for x in playerCards: 
+			if len(x) == 2:
+				print(capital(x[0]),str(x[1]))
+			else:
+				print(capital(x))
+		input("Ses")
